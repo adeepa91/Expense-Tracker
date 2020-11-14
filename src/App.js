6b33a6708a8239b601b2f2d5999231c52151c50f
Adeepa <adeepa91@gmail.com>
2020-11-14 23:34:30 +0530

    Add layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Header } from './components/Header';
+import {Balance} from './components/Balance';
+import {IncomeExpences} from './components/IncomeExpences';
+import {TransactionList} from './components/TransactionList';
+import {AddTransaction} from './components/AddTransaction';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+      <Header />
+      <div className="container">
+        <Balance/>
+        <IncomeExpences/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
